072b4f92968be5a1eb889e28d8e2b979ab1f46de
u24614582 <u24614582@tuks.co.za>
2026-09-12 21:25:11 +0200

    updated parse exception format to follow decisions in pdf

--- src/main/java/spl/parser/ParserException.java
+++ src/main/java/spl/parser/ParserException.java
@@ -1,4 +1,14 @@
 package spl.parser;
 
-public class ParserException {
+public class ParserException extends RuntimeException {
+    public final int line;
+    public final String expected;
+    public final String found;
+
+    public ParserException(int line, String expected, String found) {
+        super("Error at line " + line + ": unexpected token. Expected '" + expected + "', but found '" + found + "'.");
+        this.line = line;
+        this.expected = expected;
+        this.found = found;
+    }
 }
