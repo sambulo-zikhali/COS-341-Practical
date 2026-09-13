18608afaa9614b6fc9076338454062e02f20bb4a
u24614582 <u24614582@tuks.co.za>
2026-09-13 20:11:08 +0200

    added stub for parse table method call

--- src/main/java/spl/grammar/ParsingTable.java
+++ src/main/java/spl/grammar/ParsingTable.java
@@ -1,4 +1,11 @@
 package spl.grammar;
 
+import spl.model.ParseAction;
+import spl.model.TokenType;
+
 public class ParsingTable {
+    // stub for function returning parse action for a given token and dfa state
+    public ParseAction get(int state, TokenType type) {
+        return new ParseAction(null, state);
+    }
 }
