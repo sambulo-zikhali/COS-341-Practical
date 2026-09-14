6d12af325eac44bcfa868a0d6270cc8654cbbe51
u24614582 <u24614582@tuks.co.za>
2026-09-15 00:07:34 +0200

    added basic setup with for parser class. with constructor and getter methods as well as local vars required

--- src/main/java/spl/parser/Parser.java
+++ src/main/java/spl/parser/Parser.java
@@ -1,4 +1,46 @@
 package spl.parser;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+import spl.grammar.ParsingTable;
+import spl.model.Token;
+import spl.model.TokenType;
+import spl.model.TreeNode;
+
 public class Parser {
+    private static final Token EOF_TOKEN = new Token(TokenType.EOF, "EOF", -1);
+
+    private final ParsingTable table;
+    private final NodeIdGenerator ids;
+
+    private final Map<Integer, TreeNode> nodesById = new HashMap<>();
+
+    public Parser(ParsingTable table, NodeIdGenerator ids) {
+        this.table = table;
+        this.ids = ids;
+    }
+
+    public Map<Integer, TreeNode> getNodesById() {
+        return nodesById;
+    }
+
+    public TreeNode parse(List<Token> tokenList) {
+        nodesById.clear();
+
+        Deque<Token> tokens = new ArrayDeque<>(tokenList);
+
+        Stack<Integer> stateStack = new Stack<>();
+        Stack<Integer> nodeStack = new Stack<>();
+
+        Token lookahead = tokens.isEmpty() ? EOF_TOKEN : tokens.peek();
+
+        while (true) {
+
+        }
+    }
 }
