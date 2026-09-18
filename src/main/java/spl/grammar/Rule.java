f0a426b487ead7ca780ad3c8c29a307f628d5a2b
u24614582 <u24614582@tuks.co.za>
2026-09-18 16:05:05 +0200

    added stubs for rule

--- src/main/java/spl/grammar/Rule.java
+++ src/main/java/spl/grammar/Rule.java
@@ -1,4 +1,42 @@
 package spl.grammar;
 
+import java.util.List;
+
 public class Rule {
+    List<String> rhs;
+
+    public int getId() {
+        return -1;
+    }
+
+    public String getLhs() {
+        return "";
+    }
+
+    public List<String> getRhs() {
+        return rhs;
+    }
+
+    public int length() {
+        return -1;
+    }
+
+    public boolean isEpsilon() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "-1";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return -1;
+    }
 }
