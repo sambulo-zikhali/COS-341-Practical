b78640b233cac8b22ad160704fa134c0549d9c7e
u24614582 <u24614582@tuks.co.za>
2026-09-18 16:01:13 +0200

    updated stubs for parsing table methods

--- src/main/java/spl/grammar/ParsingTable.java
+++ src/main/java/spl/grammar/ParsingTable.java
@@ -1,11 +1,29 @@
 package spl.grammar;
 
+import java.util.Map;
+
 import spl.model.ParseAction;
 import spl.model.TokenType;
 
 public class ParsingTable {
-    // stub for function returning parse action for a given token and dfa state
-    public ParseAction get(int state, TokenType type) {
-        return new ParseAction(null, state);
+    // stubs for table endpoints
+    public int getStartState() {
+        return -1;
+    }
+
+    public ParseAction get(int state, TokenType token) {
+        return ParseAction.error();
+    }
+
+    public int getGoto(int state, String nonTerminal) {
+        return -1;
+    }
+
+    public Rule getRule(int ruleId) {
+        return new Rule();
+    }
+
+    public int numberOfStates() {
+        return -1;
     }
 }
